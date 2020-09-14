8ed4db40262357cda623f387f7017e1623a3183d
taneishi <taneishi@users.noreply.github.com>
2020-09-14 04:37:07 -0700

    rename model to net

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ import timeit
 
 from model import CompoundProteinInteractionModel
 
-def train(model, dataset, optimizer, loss_function, epoch):
-    model.train()
+def train(net, dataset, optimizer, loss_function, epoch):
+    net.train()
     train_loss = 0
     for index, (fingerprints, adjacency, words, interaction) in enumerate(dataset, 1):
         optimizer.zero_grad()
-        output = model.forward(fingerprints, adjacency, words)   
+        output = net.forward(fingerprints, adjacency, words)
         loss = loss_function(output, interaction)
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
         
-        print('\repoch %3d batch %4d/%4d train_loss %5.3f' % (epoch, index, len(dataset), train_loss / index), end='')
+    print('epoch %3d batch %4d/%4d train_loss %5.3f' % (epoch, index, len(dataset), train_loss / index), end='')
 
-def test(model, dataset, loss_function):
-    model.eval()
+def test(net, dataset, loss_function):
+    net.eval()
     test_loss = 0
     y_score, y_true = [], []
     for index, (fingerprints, adjacency, words, interaction) in enumerate(dataset, 1):
         with torch.no_grad():
-            output = model.forward(fingerprints, adjacency, words)
+            output = net.forward(fingerprints, adjacency, words)
 
         loss = loss_function(output, interaction)
         test_loss += loss.item()
@@ -45,7 +45,7 @@ def test(model, dataset, loss_function):
     prec = precision_score(y_true, y_pred)
     recall = recall_score(y_true, y_pred)
 
-    print(' test_loss %5.3f' % (test_loss / index), end='')
+    print(' test_loss %5.3f test_auc %5.3F test_prec %5.3f test_recall %5.3f' % (test_loss / index, auc, prec, recall), end='')
 
     return test_loss / index
 
@@ -75,16 +75,16 @@ def main(args):
     print('train %d test %d' % (len(dataset_train), len(dataset_test)))
     
     # Set a model
-    model = CompoundProteinInteractionModel(n_fingerprint, n_word, args)
-    model = model.to(device)
+    net = CompoundProteinInteractionModel(n_fingerprint, n_word, args)
+    net = net.to(device)
 
     if args.modelfile:
-        model.load_state_dict(torch.load(args.modelfile))
+        net.load_state_dict(torch.load(args.modelfile))
 
     if torch.cuda.device_count() > 1:
-        model = torch.nn.DataParallel(model)
+        net = torch.nn.DataParallel(net)
 
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     loss_function = F.cross_entropy
 
     test_losses = []
@@ -96,35 +96,35 @@ def main(args):
         if epoch % args.decay_interval == 0:
             optimizer.param_groups[0]['lr'] *= args.lr_decay
 
-        train(model, dataset_train, optimizer, loss_function, epoch)
-        test_loss = test(model, dataset_test, loss_function)
+        train(net, dataset_train, optimizer, loss_function, epoch)
+        test_loss = test(net, dataset_test, loss_function)
 
         print(' %5.2f sec' % (timeit.default_timer() - epoch_start))
 
         test_losses.append(test_loss)
 
         if test_loss <= min(test_losses):
-            torch.save(model.state_dict(), 'model/%5.3f.pth' % test_loss)
+            torch.save(net.state_dict(), 'model/%5.3f.pth' % test_loss)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('modelfile', nargs='?')
     parser.add_argument('--dataset', default='human', choices=['human', 'celegans'])
+    parser.add_argument('--modelfile', default=None)
     parser.add_argument('--radius', default=2, choices=[1, 2, 3])
     parser.add_argument('--ngram', default=3, choices=[2, 3])
-    parser.add_argument('--dim', default=10)
-    parser.add_argument('--layer_gnn', default=3)
-    parser.add_argument('--side', default=5)
-    parser.add_argument('--window', default=2*5+1) # 2*side+1
-    parser.add_argument('--layer_cnn', default=3)
-    parser.add_argument('--layer_output', default=3)
-    parser.add_argument('--lr', default=1e-3)
-    parser.add_argument('--lr_decay', default=0.5)
-    parser.add_argument('--decay_interval', default=10)
-    parser.add_argument('--weight_decay', default=1e-6)
-    parser.add_argument('--epochs', default=100)
+    parser.add_argument('--dim', default=10, type=int)
+    parser.add_argument('--layer_gnn', default=3, type=int)
+    parser.add_argument('--side', default=5, type=int)
+    parser.add_argument('--window', default=2*5+1, type=int) # 2*side+1
+    parser.add_argument('--layer_cnn', default=3, type=int)
+    parser.add_argument('--layer_output', default=3, type=int)
+    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--lr_decay', default=0.5, type=float)
+    parser.add_argument('--decay_interval', default=10, type=int)
+    parser.add_argument('--weight_decay', default=1e-6, type=float)
+    parser.add_argument('--epochs', default=100, type=int)
     parser.add_argument('--cpu', default=False, action='store_true')
-    parser.add_argument('--seed', default=123)
+    parser.add_argument('--seed', default=123, type=int)
     args = parser.parse_args()
     print(vars(args))
 
